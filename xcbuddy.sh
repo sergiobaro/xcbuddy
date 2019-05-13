21fdb551e4dc0f170b11bfbfb718416695a93eaa
Sergio Baro <sbaro@hubspot.com>
2019-05-13 09:14:32 +0200

    Added display simulators

--- xcbuddy.sh
+++ xcbuddy.sh
@@ -2,24 +2,33 @@
 
 operation=$1
 
-# Print command line tools current path
-if [ -z $operation ] || [ $operation = "-p" ]; then
-  xcode-select -p
-  exit 0
+if [ -z $operation ]; then
+  operation="-h"
 fi
 
-# Help
+# Display help
+# usage: xcbuddy -h
 if [ $operation = "-h" ]; then
   echo "usage:"
   echo "-h : Prints help"
   echo "-p : Prints current Xcode path"
   echo "-s [xcode_version] : Change command line tools"
   echo "-o [xcode_version] [project_file]: Open project with the specified Xcode version"
+  echo "-d : Shows Xcode installed versions"
+  echo "-m : Display available simulators"
 
   exit 0
 fi
 
+# Print command line tools current path
+# usage: xcbuddy -p
+if [ $operation = "-p" ]; then
+  xcode-select -p
+  exit 0
+fi
+
 # Change command line tools
+# usage: xcbuddy -s [version]
 if [ $operation = "-s" ]; then
   if [[ -z $2 ]]; then
     echo "Missing Xcode version"
@@ -34,16 +43,25 @@ if [ $operation = "-s" ]; then
 fi
 
 # Open Xcode project with an specific version
+# usage: xcbuddy -o [version] [project]
 if [ $operation = "-o" ]; then
-  if [[ -z $2 ]]; then
-    xcode_app_name=`xcode-select -p`
-    open -a $xcode_app_name
-    exit 0
-  fi
-
   xcode_version=$2
   xcode_app_name="Xcode_$xcode_version.app"
   project=$3
   open -a $xcode_app_name $project
+  exit 0
+fi
+
+# Show Xcode installed versions
+# usage: xcbuddy -d
+if [ $operation = "-d" ]; then
+  ls /Applications | grep "Xcode"
+  exit 0
+fi
+
+# Display simulators
+# usage: xcbuddy -m
+if [ $operation = "-m" ]; then
+  xcrun simctl list devices available
   exit 0
 fi
